054cfd087b666ed6eef3dbd4eefa9061aacc5dc6
fibo <casati_gianluca@yahoo.it>
2019-12-25 23:31:24 +0100

    fix routing for temporary demo URL

--- docs/Root.js
+++ docs/Root.js
@@ -15,6 +15,9 @@ export default function Root () {
       <Switch>
         <Route component={Design} exact path={'/'} />
 
+        {/* TODO remove this route, it is a temporary workaround to make demo work on GitHub pages until react-deisgn.dev domain is DNS propagated */}
+        <Route component={Design} exact path={'/react-desing'} />
+
         <Route component={DesignNotFound} exact path={'/design-not-found'} />
 
         <Route component={Page1} exact path={Page1.path} />
