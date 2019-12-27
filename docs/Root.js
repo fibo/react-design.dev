5a5a13e9ce8524bafb39b1347c9f5438b9419756
fibo <casati_gianluca@yahoo.it>
2019-12-27 14:22:16 +0100

    add 404 page

--- docs/Root.js
+++ docs/Root.js
@@ -15,9 +15,6 @@ export default function Root () {
       <Switch>
         <Route component={Design} exact path={'/'} />
 
-        {/* TODO remove this route, it is a temporary workaround to make demo work on GitHub pages until react-deisgn.dev domain is DNS propagated */}
-        <Route component={Design} exact path={'/react-design'} />
-
         <Route component={DesignNotFound} exact path={'/design-not-found'} />
 
         <Route component={Page1} exact path={Page1.path} />
